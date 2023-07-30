b311eef9fc677b0f333c9016874260a0dc6eea74
Julja Vysotska <julja.vysotska@gmail.com>
2023-07-30 12:13:04 +0300

    add custom hook for sort

--- src/elements/SortableTable/index.js
+++ src/elements/SortableTable/index.js
@@ -1,61 +1,21 @@
-import { useState } from "react";
 import { Table } from "../Table";
 import { getIcon } from "./getIcon";
+import { useSort } from "../../hooks/useSort";
 
 export const SortableTable = (props) => {
   const { config, data } = props;
-  let sortedData = data;
 
-  const [sortOrder, setSortOrder] = useState(null); // 'asc' 'desc' null
-  const [sortBy, setSortBy] = useState(null); // 'asc' 'desc' null
-
-  const handleClick = (label) => {
-
-    if(label !== sortBy && label ) {
-        setSortOrder("asc");
-        setSortBy(label);
-        return;
-
-    }
-    if (sortOrder === null) {
-      setSortOrder("asc");
-      setSortBy(label);
-    }
-
-    if (sortOrder === "asc") {
-      setSortOrder("desc");
-      setSortBy(label);
-    }
-
-    if (sortOrder === "desc") {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
-
-  if (sortBy && sortOrder) {
-    const { sortValue } = config.find((item) => item.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valA = sortValue(a);
-      const valB = sortValue(b);
-
-      const reversed = sortOrder === "asc" ? 1 : -1;
-
-      if (typeof valA === "string") {
-        return valA.localeCompare(valB) * reversed;
-      }
-
-      return (valA - valB) * reversed;
-    });
-  }
+  const { sortBy, sortOrder, sortedData, handleClick } = useSort(data, config);
 
   const updatedConfig = config.map((configItem) => {
     if (configItem.sortValue) {
       return {
         ...configItem,
         header: () => (
-          <th className ='cursor-pointer ' onClick={() => handleClick(configItem.label)}>
-
+          <th
+            className="cursor-pointer "
+            onClick={() => handleClick(configItem.label)}
+          >
             {getIcon(configItem.label, sortBy, sortOrder)}
             {configItem.label}
           </th>
--- src/pages/TablePage/data.js
+++ src/pages/TablePage/data.js
@@ -3,5 +3,4 @@ export const fruits = [
     {name: 'apple', color: 'bg-red-500', score: 3},
     {name: 'banana', color: 'bg-yellow-500', score: 4},
     {name: 'pear', color: 'bg-green-500', score: 1},
-    {name: 'pear', color: 'bg-green-500', score: 1.3},
 ]
